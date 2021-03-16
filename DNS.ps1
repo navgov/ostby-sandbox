bee1a2fa092413074f532d25b7595addcc9246e0
Marius Østby <51474245+mariusostby@users.noreply.github.com>
2021-03-16 17:34:56 +0100

    Update DNS.ps1

--- DNS.ps1
+++ DNS.ps1
@@ -1,3 +1,3 @@
-Install-WindowsFeature DNS -IncludeManagementTools
+Install-WindowsFeature DNS -IncludeManagementTools -erroraction SilentlyContinue
 Add-DnsServerForwarder -IPAddress 168.63.129.16 -PassThru
 Add-DnsServerConditionalForwarderZone -Name "nav.no" -MasterServers 8.8.8.8,8.8.4.4 -PassThru
